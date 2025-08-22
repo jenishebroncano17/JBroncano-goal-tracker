d29920a6064e2c787d8c3e9325dccdcd57bbf17c
“jenishebroncano” <jenishebroncano@gmail.com>
2025-08-22 16:46:31 +0800

    index is change

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        M Y  G O A L S
+         T I M E T A S K
       </Text>
       <Link style={styles.link} href="/goals">
         View Your Goals
